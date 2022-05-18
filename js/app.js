29c42c7ed97033947ffbd890620159acdf1fdbd9
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-18 22:04:21 +0500

    add 3 questions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
-//! question 1
+//! ======= question 1
 /* 
 * Which JavaScript expressions will  return NaN?
+
+?parseint("a123") and NaN ** 2
+
+*/
+
+//! =======  question 2
+
+/* 
+* What is the output of the following JavaScript code?
+let number = 0
+console.log(number++)
+console.log(++number)
+console.log(number)
+
+? 0 , 1 , 2
+*/
+
+//! =======  question 3
+
+/* 
+ * What is the output of the following JavaScript code?
+
 
 */
