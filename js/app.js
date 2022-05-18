6f0b7b685b67433d7ba95f39f1fbbe398792ded0
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-18 22:34:27 +0500

    I'm added question 5

--- js/app.js
+++ js/app.js
@@ -42,3 +42,14 @@ console.log(number)
  ?setAttribute
 
 */
+
+//! =======  question 5
+
+/*  
+ * What are the ways to create an array from digis ,
+ * 1,2 and 3 as strings in JavaScript?
+ 
+ ?Array.from('123') , ["1" , "2" , "3"] , new Array("1","2","3")
+ 
+
+*/
