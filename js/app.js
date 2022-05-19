1fd1ec7a0dae669fa0a1fb006f3b6c6e0a5035e2
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 11:43:27 +0500

    I'm add question 27

--- js/app.js
+++ js/app.js
@@ -242,3 +242,12 @@ let str = '42'
 
 ? join , push , map 
 */
+
+//! ===  question 27
+
+/* 
+* How can you select an existing DOM element?
+
+?document.getElement("div") , document querySelector("p") ,  document.getElementClassName("class") ,  document.getElementByTagName("li") ,  document.getElementByid("id")
+
+*/
