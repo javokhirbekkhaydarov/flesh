e5675712476b5a78a9d8bfd5e5d58a6ec775182a
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 05:45:27 +0500

    I'm added question-9

--- js/app.js
+++ js/app.js
@@ -77,3 +77,12 @@ console.log(number)
 */
 
 //! =======  question 9
+
+/* 
+* Which method will modify the original array in JavaScript
+
+?var a = [1, 2, 3];
+?a.splice(1,2,3)
+?console.log(a);
+
+*/
