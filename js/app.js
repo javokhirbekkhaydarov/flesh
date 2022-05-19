21889ea7f3ffee7eddf27e0b3c47445f08cf140c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 06:32:03 +0500

    I'm added question 16

--- js/app.js
+++ js/app.js
@@ -136,3 +136,12 @@ document.getElementById("root")
 
  ?super method
  */
+
+//! ===  question 16
+
+/*
+ * How can an event handler be assigned to an element in a browser?
+
+?use addEventListener() method and HTML event handler attribute
+
+ */
