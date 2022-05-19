99b34697cb662cedcd16f6910844dfb7f57a01fe
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 06:47:58 +0500

    I'm added question 18

--- js/app.js
+++ js/app.js
@@ -154,3 +154,18 @@ document.getElementById("root")
 ?let str = "stringify" (str.slice(2,6));
 
 */
+
+//! ===  question 18
+
+/* 
+* Which code will return undefined after the followig JavaScript decloration
+
+var obj = {
+  a: 1,
+  b: { c: 2 },
+};
+?console.log(obj.c);
+?console.log(obj?.a?.b?.c?.d);
+
+
+*/
