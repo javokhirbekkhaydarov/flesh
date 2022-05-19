1013c368661fdf603fd957a840b7dd2d5fdcb440
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 11:19:35 +0500

    I'm added question 23

--- js/app.js
+++ js/app.js
@@ -215,3 +215,14 @@ let str = '42'
 
 
 */
+
+//! ===  question 23
+
+/* 
+* Which of the following quantifiers means  zero to five occurrences in regular expressions?
+[0 ..5]
+[0<5]
+[0<5]
+[0-5]
+
+*/
