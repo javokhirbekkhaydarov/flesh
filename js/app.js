1b031e37a5bc76c0ad97d43167556cef74bb444c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 09:51:23 +0500

    I'm added question 19

--- js/app.js
+++ js/app.js
@@ -169,3 +169,19 @@ var obj = {
 
 
 */
+
+while (noSucces) {
+  tryAgain();
+  if (Dead) break;
+}
+//! Javohirbek
+
+//! ===  question 19
+
+/* 
+
+* What syntac says that is the X class implements the Y interface in TypeScript
+
+? X inherits Y this is approximate
+
+*/
