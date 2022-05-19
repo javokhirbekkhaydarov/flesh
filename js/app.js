f54d3eb9a77f44c46bbfcab986d414d7349ddbed
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-19 11:31:57 +0500

    I'm add question 25

--- js/app.js
+++ js/app.js
@@ -226,3 +226,11 @@ let str = '42'
 [0-5]
 
 */
+
+//! ===  question 25
+/* 
+* What are the ways to acces obj's proporty named prop in JavaScript?
+
+?bracket notation and dot notation obj["prop"] const {prop} = obj 
+
+*/
