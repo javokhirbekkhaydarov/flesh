483ce186486f6821740823e4286bd701cd351f40
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-20 21:06:38 +0500

    I'm add question 39

--- js/app.js
+++ js/app.js
@@ -351,3 +351,17 @@ console.log(a==b)
 ?element.addEventListener
 
 */
+
+//! ===  question 39
+
+/* 
+* When will the if body be executed in JavaScript
+
+?if (condition) {
+   statement1
+} else {
+   statement2
+}
+ 
+
+*/
