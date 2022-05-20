d4e7c57ac68c3743ef78f16279ebdc3c5ff4995d
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-20 21:35:07 +0500

    I'm add question 41

--- js/app.js
+++ js/app.js
@@ -380,3 +380,12 @@ console.log(b === e);
 
 ? true false false
  */
+
+//! ===  question 41
+
+/* 
+* How long is the data available in localStorage in a browser?
+
+?forever, the data doesn't get lost
+
+*/
