59fe068290fb2f02edf164b4a412940f3f88ad2c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-20 21:16:42 +0500

    I'm add question 40

--- js/app.js
+++ js/app.js
@@ -365,3 +365,18 @@ console.log(a==b)
  
 
 */
+
+//! ===  question 40
+/*
+ * What is the output of the following JavaScript code?
+
+let a = 3;
+let b = new Number(3);
+
+let e = 3;
+console.log(a == b);
+console.log(a === b);
+console.log(b === e);
+
+? true false false
+ */
