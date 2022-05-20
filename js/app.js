f956bd35fa88a6688bb4b26e564d5c6c61d8f7f4
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-20 20:53:32 +0500

    I'm add question 38

--- js/app.js
+++ js/app.js
@@ -342,3 +342,12 @@ console.log(a==b)
 
 ? i guess true
 */
+
+//! ===  question 38
+
+/* 
+* What method do we use to add an event listener in JavaScript being executed in a browser?
+
+?element.addEventListener
+
+*/
