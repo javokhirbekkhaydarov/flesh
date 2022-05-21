7dc6d0b385385354fb3835526c555c5f05d6e251
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-21 22:15:46 +0500

    I'm added question 58

--- js/app.js
+++ js/app.js
@@ -540,3 +540,11 @@ console.log(bound(3, 4, 5));
  
  ?undefined
  */
+
+//! ===  question 58
+
+/* 
+*Which ways to create  a string are valid in JavaScript
+
+? let str = 42.toString let str = '42' , let str = "42"
+*/
