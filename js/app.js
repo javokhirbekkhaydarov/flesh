6f04a2f559a61dec1bcb72e93538ae5dd2c7ecb4
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-21 21:46:28 +0500

    I'm added question 48

--- js/app.js
+++ js/app.js
@@ -443,3 +443,16 @@ const all = Promise.all([timer(" first "), timer("second")]).then((data) =>
 ?getComputedStyle to get the element's styles
 
 */
+
+//! ===  question 48
+
+/*
+ *What is the output of the folowing JavaScript code
+ for (var i = 0; i < 5; i++) {
+   setTimeout(function () {
+     console.log(i);
+   }, i * 1000);
+ }
+
+ ?5,5,5,5,5
+ */
