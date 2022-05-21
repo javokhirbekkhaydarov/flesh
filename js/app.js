d91226283d3bf263f9ecfcf8ee725366942ed7db
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-21 22:19:27 +0500

    I'm added question 60 that is last question

--- js/app.js
+++ js/app.js
@@ -548,3 +548,15 @@ console.log(bound(3, 4, 5));
 
 ? let str = 42.toString let str = '42' , let str = "42"
 */
+
+//! ===  question 60
+
+/* 
+*What value will variable i have after the end of the loop in JavaScript
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+? 1,2,3,4,5,6,7,8,9
+
+*/
