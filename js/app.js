658ee0a68c3cc0ec34947c94819c4a2437113536
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-05-21 22:08:43 +0500

    I'm added question 55

--- js/app.js
+++ js/app.js
@@ -496,3 +496,26 @@ get("your clothes,  your boots and your motoreycle request");
 
 ? / (a-zA-Z)+/
 */
+
+//! ===  question 55
+
+/* 
+*What is the output of the following JavaScript code?
+
+?14,14,14
+
+*/
+
+let obj = {
+  x: 2,
+};
+
+let add = function (a, b, c) {
+  return this.x + a + b + c;
+};
+
+let arr = [3, 4, 5];
+let bound = add.bind(obj);
+console.log(add.call(obj, 3, 4, 5));
+console.log(add.apply(obj, arr));
+console.log(bound(3, 4, 5));
