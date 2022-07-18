db9be5ebdbd913a66d227a241b5ff1fea0634e87
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-18 17:24:27 +0500

    solved question1

---
+++ js/test.js
@@ -0,0 +1,7 @@
+console.log(parseInt(`a123`));
+console.log(5/0);
+console.log(0/0);
+console.log(`Nan ** 2`);
+console.log(Infinity + 1);
+
+// this is question1 answear => parseInt(`a123`) and 0/0
