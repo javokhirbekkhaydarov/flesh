f1774a3c3b2195b7a67a12beefa797ebf509f991
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-19 22:12:48 +0500

    splice

--- js/test.js
+++ js/test.js
@@ -1,10 +1,10 @@
+//! =======  question 9
+
 /* 
-* Which JavaScript statements will output true?
+* Which method will modify the original array in JavaScript
 
-? (5 + 10 * 3 === 45) and (5 % 1 === 0) these statiments true
+
 */
-
-console.log(5 + 10 * 3 === 45);
-console.log(2 ** (3 ** 2) === 512);
-console.log(5 % 1 === 0);
-console.log(8 + 8 / 2 + 2 === 10);
+var a = [1, 2, 3];
+a.splice(1,2,3)
+console.log(a);
