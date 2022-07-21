c7122637bad312c70c16c5cf0e36b622eddf92a7
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-21 19:21:50 +0500

    settimeout and angular

--- js/test.js
+++ js/test.js
@@ -1,9 +1,29 @@
-//! ===  question 32
+//! ===  question 33
 
 /* 
-*Which of the following is the correct syntax to create a cookie using JavaScript in a Browser?
+* What is the output of the following JavaScript code?
 
-? document.cookie = "username= Zefram Cochrane"
-this is solved
+?0,1,2,3,4
 
+*/
+
+for(let i = 0; i < 5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, i * 1000);
+}
+
+//! ===  question 34
+
+/* 
+* How can you configure the template of the component in Angular
+
+
+ ))@Component({
+  selector: 'app-component-overview',
+})
+ @Component({
+  selector: 'app-component-overview',
+  templateUrl: './component-overview.component.html',
+})
 */
