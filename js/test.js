b47bddddc8c02ff032832c105dd57ad5c207cdfd
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-21 19:23:15 +0500

    queryselector'

--- js/test.js
+++ js/test.js
@@ -1,29 +1,8 @@
-//! ===  question 33
+//! ===  question 35
 
-/* 
-* What is the output of the following JavaScript code?
+/*
+ *Which method selects the first matching element in the document
 
-?0,1,2,3,4
+ ?querySelector()
 
-*/
-
-for(let i = 0; i < 5; i++) {
-    setTimeout(() => {
-        console.log(i);
-    }, i * 1000);
-}
-
-//! ===  question 34
-
-/* 
-* How can you configure the template of the component in Angular
-
-
- ))@Component({
-  selector: 'app-component-overview',
-})
- @Component({
-  selector: 'app-component-overview',
-  templateUrl: './component-overview.component.html',
-})
-*/
+ */
