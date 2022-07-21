8cbbf00a2d9b79dc54311e417175b713476d32a3
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-21 10:52:56 +0500

    ring

--- js/test.js
+++ js/test.js
@@ -1,3 +1,14 @@
+//! ===  question 17
+
+/* 
+* How to extract "ring " part from the following JavaScript string?
+
+?let str = "stringify" (str.slice(2,6));
+
+*/
+
+
+
 //! ===  question 18
 
 /* 
