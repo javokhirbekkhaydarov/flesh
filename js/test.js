4f587f47bffeaefe80f8f4e892ff3a901f09da3c
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-23 13:31:39 +0500

    14,14,14

--- js/test.js
+++ js/test.js
@@ -1,7 +1,36 @@
-//! ===  question 54
-/* 
-*Which of the following regular  expressions 2matches all of these substrings
 
-? / (a-zA-Z)+/
-? / (a-zA-Z)*
+//! ===  question 55
+
+/* 
+*What is the output of the following JavaScript code?
+let obj = {
+  x: 2,
+};
+
+let add = function (a, b, c) {
+  return this.x + a + b + c;
+};
+
+let arr = [3, 4, 5];
+let bound = add.bind(obj);
+console.log(add.call(obj, 3, 4, 5));
+console.log(add.apply(obj, arr));
+console.log(bound(3, 4, 5));
+
+?14,14,14
+
 */
+
+let obj = {
+  x: 2,
+};
+
+let add = function (a, b, c) {
+  return this.x + a + b + c;
+};
+
+let arr = [3, 4, 5];
+let bound = add.bind(obj);
+console.log(add.call(obj, 3, 4, 5));
+console.log(add.apply(obj, arr));
+console.log(bound(3, 4, 5));
