6f593b89b00bb09a83861cb2303eadc1299f7b95
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-23 13:33:56 +0500

    dot and brecket

--- js/test.js
+++ js/test.js
@@ -1,36 +1,9 @@
-
-//! ===  question 55
+//! ===  question 56
 
 /* 
-*What is the output of the following JavaScript code?
-let obj = {
-  x: 2,
-};
+*Which ways to add a property for the existing object are correct in JavaScript
 
-let add = function (a, b, c) {
-  return this.x + a + b + c;
-};
-
-let arr = [3, 4, 5];
-let bound = add.bind(obj);
-console.log(add.call(obj, 3, 4, 5));
-console.log(add.apply(obj, arr));
-console.log(bound(3, 4, 5));
-
-?14,14,14
+? obj.a = 1 obj[a] =1
 
 */
-
-let obj = {
-  x: 2,
-};
-
-let add = function (a, b, c) {
-  return this.x + a + b + c;
-};
-
-let arr = [3, 4, 5];
-let bound = add.bind(obj);
-console.log(add.call(obj, 3, 4, 5));
-console.log(add.apply(obj, arr));
-console.log(bound(3, 4, 5));
+ 
