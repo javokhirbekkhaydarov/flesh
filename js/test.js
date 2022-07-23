11fc42513912cbd78c297705f5e2b89e8b479c10
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-23 13:44:38 +0500

    1,2,23,4,,5,6,7,8,9

--- js/test.js
+++ js/test.js
@@ -1,14 +1,12 @@
-//! ===  question 59
+
+//! ===  question 60
 
 /* 
-*What is the output of the following TypeScript code?
-
-class Counter {
-  static count: number = 0;
+*What value will variable i have after the end of the loop in JavaScript
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
-const counter1 = new Counter();const counter2 = new Counter();
-counter1.count = 1;
-console.log(counter1.count, counter2.count)
 
-? 1 ,  undefined
+? 1,2,3,4,5,6,7,8,9
+
 */
